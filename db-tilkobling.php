1d702d4544d04bc70e0fcd2ca3247c0be005c97e
TropicalBananaMan <mrmineturtle15@gmail.com>
2018-03-24 20:50:50 +0100

    Fikset db-tilkobling.php

--- db-tilkobling.php
+++ db-tilkobling.php
@@ -1,3 +1,3 @@
 <?php
 
-$db = mysqli_connect('127.0.0.1', 'root', '', 'gfes') or die ("fikk ikke kontakt med db");
+$db = mysqli_connect('127.0.0.1', 'root', '', 'gfas') or die ("fikk ikke kontakt med db");
